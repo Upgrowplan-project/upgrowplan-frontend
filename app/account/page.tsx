41905a4654fe2f90214deb82dcafe84a36bb80f8
DenisNaletov <naletov@bk.ru>
2025-08-16 17:47:28 +0300

    login, account pages added

---
+++ app/account/page.tsx
@@ -0,0 +1,11 @@
+"use client"; // если есть useState или другие хуки
+
+import React from "react";
+
+export default function AccountPage() {
+  return (
+    <div>
+      <h1>Страница аккаунта</h1>
+    </div>
+  );
+}
