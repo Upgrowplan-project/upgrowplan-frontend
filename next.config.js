03e82cb62d45cb2ddb89276bdbc10c248ee2fbf3
DenisNaletov <naletov@bk.ru>
2025-10-07 22:17:54 +0300

    blog page updated

--- next.config.js
+++ next.config.js
@@ -1,22 +1,20 @@
 /** @type {import('next').NextConfig} */
-const dotenv = require("dotenv");
-dotenv.config();
-
 const isDev = process.env.NODE_ENV !== "production";
 
 // базовые адреса
 const API_BASE_URL = isDev
   ? "http://localhost:8082"
-  : "https://upgrowplan-user-service-3b483837144a.herokuapp.com";
+  : "https://blog-service-14ba37a6adc0.herokuapp.com";
 
 const WS_BASE_URL = isDev
   ? "http://localhost:8082/ws"
-  : "https://upgrowplan-user-service-3b483837144a.herokuapp.com/ws";
-
-process.env.NEXT_PUBLIC_API_BASE_URL = API_BASE_URL;
-process.env.NEXT_PUBLIC_WS_BASE_URL = WS_BASE_URL;
+  : "https://blog-service-14ba37a6adc0.herokuapp.com/ws";
 
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: API_BASE_URL,
+    NEXT_PUBLIC_WS_BASE_URL: WS_BASE_URL,
+  },
   async rewrites() {
     return [
       {
